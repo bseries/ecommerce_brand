53c8d75482b9bd241a76e4c14bbf180b502f19ef
David Persson <davidpersson@gmx.de>
2015-07-19 18:07:54 +0200

    Use non static actsAs.

--- models/Brands.php
+++ models/Brands.php
@@ -27,7 +27,7 @@ class Brands extends \base_core\models\Base {
 		]
 	];
 
-	protected static $_actsAs = [
+	public $actsAs = [
 		'base_core\extensions\data\behavior\Sluggable',
 		'base_media\extensions\data\behavior\Coupler' => [
 			'bindings' => [
